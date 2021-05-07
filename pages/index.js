387b4468ffeb0b0a4d22f18a147c1a5493f2866d
Zach Schickler <zachschickler@knights.ucf.edu>
2021-05-07 05:47:40 -0400

    updated landing page

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,17 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Schickler</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Hi, my name is
+        </p>
+        <Header title="Zach Schickler. \nI make games." />
+        <p className="description">
+          I make games.
         </p>
       </main>
 
